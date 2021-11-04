76e4a67a5275f46e7f55083f87661202e93661dc
Ivan Nikitin <iva3nik@yandex.ru>
2021-11-04 18:32:01 +0300

    feat: Slider has been added

--- nextjs-blog/components/addition/addition.js
+++ nextjs-blog/components/addition/addition.js
@@ -1,4 +1,9 @@
 import styles from "./addition.module.css"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 export default function Addition() {
   return (
@@ -11,86 +16,188 @@ export default function Addition() {
         Eu labore eiusmod duis ad cupidatat occaecat dolor anim eiusmod fugiat labore
         sunt eu.
       </p>
-      <div className={styles.addition__cards}>
-        <div className={styles.addition__card}>
-          <div className={styles.addition__visaul}>
-            <div className={styles.addition__string}>
-              <div className={styles.addition__offer}>BEST OFFER</div>
-              <button className={styles.addition__button}></button>
+      <Swiper
+        modules={[Navigation,]}
+        spaceBetween={20}
+        slidesPerView={3}
+        centeredSlides={true}
+        navigation
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <div className={styles.addition__card}>
+            <div className={`${styles.addition__visaul} ${styles.addition__diving}`}>
+              <div className={`${styles.addition__string} ${styles.addition__string_offer}`}>
+                <div className={styles.addition__offer}>BEST OFFER</div>
+                <button className={styles.addition__button}></button>
+              </div>
+              <div className={styles.addition__place}>
+                <div className={styles.addition__location}></div>
+                Country, Region
+              </div>
             </div>
-            <div className={styles.addition__place}>
-              <div className={styles.addition__location}></div>
-              Country, Region
+            <h3 className={styles.addition__subtitle}>Your Experience</h3>
+            <div className={styles.addition__block}>
+              <p className={styles.addition__price}>$ 15.000 / hour</p>
+              <button className={styles.addition__like}>
+                <div className={styles.addition__thumb}></div>
+                8
+              </button>
             </div>
+            <p className={styles.addition__subtext}>
+              Duration: from 60 min
+            </p>
+            <p className={styles.addition__subtext}>
+              Training and Riding Horses and Ponies
+            </p>
           </div>
-          <h3 className={styles.addition__subtitle}>Your Experience</h3>
-          <div className={styles.addition__block}>
-            <p className={styles.addition__price}>$ 15.000 / hour</p>
-            <button className={styles.addition__like}>
-              <div className={styles.addition__thumb}></div>
-              8
-            </button>
-          </div>
-          <p className={styles.addition__subtext}>
-            Duration: from 60 min
-          </p>
-          <p className={styles.addition__subtext}>
-            Training and Riding Horses and Ponies
-          </p>
-        </div>
-        <div className={styles.addition__card}>
-          <div className={styles.addition__visaul}>
-            <div className={styles.addition__string}>
-              <div className={styles.addition__offer}>BEST OFFER</div>
-              <button className={styles.addition__button}></button>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.addition__card}>
+            <div className={`${styles.addition__visaul} ${styles.addition__fishing}`}>
+              <div className={styles.addition__string}>
+                <div className={`${styles.addition__offer} ${styles.addition__offer_disabled}`}>
+                  BEST OFFER
+                </div>
+                <button className={styles.addition__button}></button>
+              </div>
+              <div className={styles.addition__place}>
+                <div className={styles.addition__location}></div>
+                Country, Region
+              </div>
             </div>
-            <div className={styles.addition__place}>
-              <div className={styles.addition__location}></div>
-              Country, Region
+            <h3 className={styles.addition__subtitle}>Your Experience</h3>
+            <div className={styles.addition__block}>
+              <p className={styles.addition__price}>$ 15.000 / hour</p>
+              <button className={styles.addition__like}>
+                <div className={styles.addition__thumb}></div>
+                8
+              </button>
             </div>
+            <p className={styles.addition__subtext}>
+              Duration: from 60 min
+            </p>
+            <p className={styles.addition__subtext}>
+              Training and Riding Horses and Ponies
+            </p>
           </div>
-          <h3 className={styles.addition__subtitle}>Your Experience</h3>
-          <div className={styles.addition__block}>
-            <p className={styles.addition__price}>$ 15.000 / hour</p>
-            <button className={styles.addition__like}>
-              <div className={styles.addition__thumb}></div>
-              8
-            </button>
-          </div>
-          <p className={styles.addition__subtext}>
-            Duration: from 60 min
-          </p>
-          <p className={styles.addition__subtext}>
-            Training and Riding Horses and Ponies
-          </p>
-        </div>
-        <div className={styles.addition__card}>
-          <div className={styles.addition__visaul}>
-            <div className={styles.addition__string}>
-              <div className={styles.addition__offer}>BEST OFFER</div>
-              <button className={styles.addition__button}></button>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.addition__card}>
+            <div className={`${styles.addition__visaul} ${styles.addition__balloon}`}>
+              <div className={`${styles.addition__string} ${styles.addition__string_offer}`}>
+                <div className={styles.addition__offer}>BEST OFFER</div>
+                <button className={styles.addition__button}></button>
+              </div>
+              <div className={styles.addition__place}>
+                <div className={styles.addition__location}></div>
+                Country, Region
+              </div>
             </div>
-            <div className={styles.addition__place}>
-              <div className={styles.addition__location}></div>
-              Country, Region
+            <h3 className={styles.addition__subtitle}>Your Experience</h3>
+            <div className={styles.addition__block}>
+              <p className={styles.addition__price}>$ 15.000 / hour</p>
+              <button className={styles.addition__like}>
+                <div className={styles.addition__thumb}></div>
+                8
+              </button>
             </div>
+            <p className={styles.addition__subtext}>
+              Duration: from 60 min
+            </p>
+            <p className={styles.addition__subtext}>
+              Training and Riding Horses and Ponies
+            </p>
           </div>
-          <h3 className={styles.addition__subtitle}>Your Experience</h3>
-          <div className={styles.addition__block}>
-            <p className={styles.addition__price}>$ 15.000 / hour</p>
-            <button className={styles.addition__like}>
-              <div className={styles.addition__thumb}></div>
-              8
-            </button>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.addition__card}>
+            <div className={`${styles.addition__visaul} ${styles.addition__diving}`}>
+              <div className={`${styles.addition__string} ${styles.addition__string_offer}`}>
+                <div className={styles.addition__offer}>BEST OFFER</div>
+                <button className={styles.addition__button}></button>
+              </div>
+              <div className={styles.addition__place}>
+                <div className={styles.addition__location}></div>
+                Country, Region
+              </div>
+            </div>
+            <h3 className={styles.addition__subtitle}>Your Experience</h3>
+            <div className={styles.addition__block}>
+              <p className={styles.addition__price}>$ 15.000 / hour</p>
+              <button className={styles.addition__like}>
+                <div className={styles.addition__thumb}></div>
+                8
+              </button>
+            </div>
+            <p className={styles.addition__subtext}>
+              Duration: from 60 min
+            </p>
+            <p className={styles.addition__subtext}>
+              Training and Riding Horses and Ponies
+            </p>
           </div>
-          <p className={styles.addition__subtext}>
-            Duration: from 60 min
-          </p>
-          <p className={styles.addition__subtext}>
-            Training and Riding Horses and Ponies
-          </p>
-        </div>
-      </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.addition__card}>
+            <div className={`${styles.addition__visaul} ${styles.addition__fishing}`}>
+              <div className={styles.addition__string}>
+                <div className={`${styles.addition__offer} ${styles.addition__offer_disabled}`}>
+                  BEST OFFER
+                </div>
+                <button className={styles.addition__button}></button>
+              </div>
+              <div className={styles.addition__place}>
+                <div className={styles.addition__location}></div>
+                Country, Region
+              </div>
+            </div>
+            <h3 className={styles.addition__subtitle}>Your Experience</h3>
+            <div className={styles.addition__block}>
+              <p className={styles.addition__price}>$ 15.000 / hour</p>
+              <button className={styles.addition__like}>
+                <div className={styles.addition__thumb}></div>
+                8
+              </button>
+            </div>
+            <p className={styles.addition__subtext}>
+              Duration: from 60 min
+            </p>
+            <p className={styles.addition__subtext}>
+              Training and Riding Horses and Ponies
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.addition__card}>
+            <div className={`${styles.addition__visaul} ${styles.addition__balloon}`}>
+              <div className={`${styles.addition__string} ${styles.addition__string_offer}`}>
+                <div className={styles.addition__offer}>BEST OFFER</div>
+                <button className={styles.addition__button}></button>
+              </div>
+              <div className={styles.addition__place}>
+                <div className={styles.addition__location}></div>
+                Country, Region
+              </div>
+            </div>
+            <h3 className={styles.addition__subtitle}>Your Experience</h3>
+            <div className={styles.addition__block}>
+              <p className={styles.addition__price}>$ 15.000 / hour</p>
+              <button className={styles.addition__like}>
+                <div className={styles.addition__thumb}></div>
+                8
+              </button>
+            </div>
+            <p className={styles.addition__subtext}>
+              Duration: from 60 min
+            </p>
+            <p className={styles.addition__subtext}>
+              Training and Riding Horses and Ponies
+            </p>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </section>
   )
 }
--- nextjs-blog/components/photos/photos.js
+++ nextjs-blog/components/photos/photos.js
@@ -1,10 +1,79 @@
 import styles from './photos.module.css'
+import Image from 'next/image'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 
 export default function Photos() {
   return (
-    <div className={styles.photos}>
-      <div className={`${styles.photos__card} ${styles.photos__balkony}`}></div>
-      <div className={`${styles.photos__card} ${styles.photos__balkony}`}></div>
-    </div>
+      <div className={styles.photos}>
+        <Swiper
+          modules={[Navigation,]}
+          spaceBetween={20}
+          slidesPerView={3}
+          centeredSlides={true}
+          navigation
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide>
+            <Image
+              className={styles.photos__image}
+              src="/images/bb_3.png"
+              alt="Balkony"
+              height={352}
+              width={520}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              className={styles.photos__image}
+              src="/images/bb_1.png"
+              alt="Balkony"
+              height={352}
+              width={520}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              className={styles.photos__image}
+              src="/images/bb_2.png"
+              alt="Balkony"
+              height={352}
+              width={520}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              className={styles.photos__image}
+              src="/images/bb_3.png"
+              alt="Balkony"
+              height={352}
+              width={520}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              className={styles.photos__image}
+              src="/images/bb_1.png"
+              alt="Balkony"
+              height={352}
+              width={520}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              className={styles.photos__image}
+              src="/images/bb_2.png"
+              alt="Balkony"
+              height={352}
+              width={520}
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
   )
 }
